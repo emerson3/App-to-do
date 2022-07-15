b5f2e6aeed68ce38d18fb229003542a7bd4c39d0
Emerson <emersonbljr2802@gmail.com>
2022-07-15 16:48:32 -0300

    App to-do. Adicionando components, telas e etc.

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import Colors from './Colors';
+import tempData from './tempData';
+import TodoList from './components/TodoList';
+import AddListModal from './components/AddListModal'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    addTodoVisible: false
+  }
+
+  toggleAddTodoModal() {
+    this.setState({ addTodoVisible: !this.state.addTodoVisible });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType='slide'
+          visible={this.state.addTodoVisible}
+          onRequestClose={() => this.toggleAddTodoModal()}
+        >
+          <AddListModal closeModal={() => this.toggleAddTodoModal()} />
+        </Modal>
+
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo <Text style={{ fontWeight: "300", color: Colors.blue }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
+            <AntDesign name="plus" size={16} color={Colors.blue} />
+          </TouchableOpacity>
+          <Text style={styles.add}>Add List</Text>
+        </View>
+
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={item => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (<TodoList list={item} />)}
+          />
+        </View>
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +64,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider: {
+    backgroundColor: Colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: Colors.black,
+    paddingHorizontal: 64
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: Colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  add: {
+    color: Colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8
+  }
 });
---
+++ tempData.js
@@ -0,0 +1,38 @@
+export default tempData = [
+    {
+        name: "Viagem",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Pagar casa",
+                completed: true
+            },
+            {
+                title: "Pagar carro e gasolina",
+                completed: false
+            },
+            {
+                title: "Pagar inscrição",
+                completed: false
+            }
+        ]
+    },
+    {
+    name: "Namoro",
+        color: "#8B0000",
+        todos: [
+            {
+                title: "Anel de pato",
+                completed: false
+            },
+            {
+                title: "Carta",
+                completed: false
+            },
+            {
+                title: "Decisão do local",
+                completed: true
+            }
+        ]
+    }
+]
